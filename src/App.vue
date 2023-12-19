3a109e959b861f5555e9469c0af54b81725ca70e
Aakeefah Jones <aakeefahj@gmail.com>
2023-12-19 12:34:09 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row">
+    <h1>Counter App</h1>
+    <h2>{{ cnt }}</h2>
+      <button class="btn" @click="increment">Add</button>
+      <button class="btn" @click="decrement">Subtract</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+    computed: {
+      cnt() {
+        return this.$store.state.cnt;
+      }
+    },
+    methods: {
+      increment() {
+        this.$store.dispatch('increment')
+      },
+      decrement() {
+        this.$store.dispatch('decrement')
+      },
+    }
   }
-}
+
 </script>
 
 <style>
@@ -22,5 +37,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.row{
+  background: linear-gradient(90deg, rgb(155, 187, 247) 0%, #fdcf6c 100%);
+  padding: 20px;
+  margin: auto;
+  width: 500px;
+  align-items: center;
+  border-radius: 20px;
+  box-shadow: #999 10px 10px 10px 0px;
+}
+.btn{
+  margin: 10px;
+  width: 100px;
+  padding: 5px;
+  border-radius: 10px;
+  background-color: seashell;
+  border: none;
+  font-size: small;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,25 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    // defines the variable.
+    cnt: 0
   },
   getters: {
   },
   mutations: {
+    // only updates the state by means of mutation.
+    setCounter(state, value){
+      state.cnt = value
+    }
   },
   actions: {
+    increment(context) {
+      context.commit('setCounter', ++context.state.cnt)
+    },
+    decrement(context){
+      if(context.state.cnt > 0 )
+      context.commit('setCounter', --context.state.cnt)
+    }
   },
   modules: {
   }
